23cd2f9cda70abf44c3316239af60f7ad62831ad
Malik <malik_ali1998@hotmail.co.uk>
2020-11-05 11:47:39 +0000

    MA : Operators task complete

---
+++ calculator.java
@@ -0,0 +1,43 @@
+package main;
+
+public class calculator {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		int x = 5;
+		int y = 10;
+		System.out.println(add(x, y));
+		System.out.println(minus(x, y));
+		System.out.println(multi(x, y));
+		if (x < y) {
+			System.out.println("Division cannot be performed");
+		} else {
+			System.out.println(div(x, y));
+		}
+	}
+	
+	public static int add(int x, int y) {
+		return  x + y;
+		
+	}
+	
+	public static int minus(int x, int y) {
+		return  x - y;
+		
+	}
+	
+	public static int multi(int x, int y) {
+		return  x * y;
+		
+	}
+	
+	public static double div(double x, double y) {
+		return  x / y;
+		
+	}
+	
+	
+	
+	
+
+}
