79328dfc4db79bddb5bd8026031049ae7fd953a8
Malik <malik_ali1998@hotmail.co.uk>
2020-11-05 11:50:35 +0000

    MA: Completed

---
+++ results.java
@@ -0,0 +1,31 @@
+package main;
+
+public class results {
+	static int biomarks = 100;
+	static int chemmarks = 100;
+	static int physmark = 100;
+	static int total = biomarks + chemmarks + physmark;
+	static double perc = ((biomarks + chemmarks + physmark)/3);
+	
+	public static void main(String[] args) {
+		display();
+		//percentage();
+		System.out.println("Overall Percentage: " + percentage() +"%");
+		
+	}
+	
+	public static void display() {
+		System.out.println("Biology Mark: " + biomarks);
+		System.out.println("Chemistry Mark: " + chemmarks);
+		System.out.println("Phyics Mark: " + physmark);
+		System.out.println("Total Mark: " + total);
+		
+	}
+	
+	public static double percentage() {
+		double ans = (perc / 150)*100;
+		return ans;
+		
+	}
+
+}
